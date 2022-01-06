a75670bfb838d63c459c8110a7a2441dc1d20ca5
hskkhr <hskkhr@naver.com>
2022-01-06 14:16:19 +0900

    반복횟수만큼 반복되는 프로그램 구현 - for문

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -10,31 +10,28 @@ public class Calendar {
 	public int getMaxDaysOfMonth(int month) {
 		return MAX_DAYS[month-1];
 	}
-	
-	//샘플 캘린더를 출력하는 함수
-	public void printSampleCalendar() {
-		System.out.println("일  월  화  수  목 금  토");
-		System.out.println("----------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-	}
 
 	public static void main(String[] args) {
+		//반복횟수 입력받기
+		System.out.println("반복횟수를 입력하세요.");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int[] month = new int[num];
+		
+		//반복횟수만큼 월 입력받기
+		System.out.println("월을 입력하세요.");
+		for(int i=0;i<num;i++) {
+			month[i] = sc.nextInt();
+		}
+		
 		//클래스 생성자
 		Calendar cd = new Calendar();
 		
-		//달 입력 받기
-		System.out.println("달을 입력하세요.");
-		Scanner sc = new Scanner(System.in);
-		int month = sc.nextInt();
-		
 		//결과 출력
-		System.out.printf("%d월은 %d일까지 있습니다.\n\n", month, cd.getMaxDaysOfMonth(month));
+		for(int i=0;i<num;i++) {
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month[i], cd.getMaxDaysOfMonth(month[i]));
+		}
 		sc.close();
-		
-		cd.printSampleCalendar();
 	}
 
 }
